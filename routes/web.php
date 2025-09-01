9987f1ff9e0ee6a066288ccbd80364346b25a09b
hasnainmawia <hasnainmawia.w@gmail.com>
2025-09-01 20:59:45 +0500

    dashboard-added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Auth\DashboardController;
+use App\Http\Controllers\Auth\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,11 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/logout', function () {
+    auth()->logout();
+    // return view('welcome');
 });
-Route::view('/dashboard', 'auth.dashboard');
+// Route::view('/dashboard', 'auth.dashboard');
 
 Auth::routes();
+
+Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
